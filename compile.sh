3f1b1d0c0f3f98380ec8e1a67b7d1127974e8210
Christos Papageorgakis <christos.papageorgakis@cern.ch>
2020-06-19 11:43:45 -0400

    Use system variable for fastjet installation, if available.

--- compile.sh
+++ compile.sh
@@ -1,1 +1,10 @@
-g++ -I $(pwd) -Wall $(root-config --cflags --libs) $(~/fastjet-install/bin/fastjet-config --cxxflags --libs --plugins) -o doHistos Root/doHistos.C
+# Define path variables
+export SUEP_ANALYSIS=`pwd`
+if [ -z $FASTJET_PATH ]
+then
+  export FASTJET_PATH=~/fastjet-install
+fi
+
+# Compile doHistos
+g++ -I $SUEP_ANALYSIS -Wall $(root-config --cflags --libs) $($FASTJET_PATH/bin/fastjet-config \
+    --cxxflags --libs --plugins) -o doHistos Root/doHistos.C
