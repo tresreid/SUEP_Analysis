0f7e86913517329302999817dc07ded5c9aa36fa
Christos Papageorgakis <Christos.Papageorgakis@cern.ch>
2020-07-09 20:15:44 -0400

    Add option for fastjet installation

--- setup.sh
+++ setup.sh
@@ -8,15 +8,16 @@ source /cvmfs/sft.cern.ch/lcg/views/LCG_97rc4/x86_64-centos7-gcc9-opt/setup.sh
 if [ ! -d "$SUEP_BASE/output" ]
 then
   mkdir $SUEP_BASE/output
+  echo "Created $SUEP_BASE/output directory."
 fi
 if [ ! -d "$SUEP_BASE/plots" ]
 then
   mkdir $SUEP_BASE/plots
+  echo "Created $SUEP_BASE/plots directory."
 fi
 
 # Install fastjet locally
-if [ ! -d "$SUEP_BASE/fastjet-install"]
-then
+function FASTJET {
   curl -O http://fastjet.fr/repo/fastjet-3.3.4.tar.gz
   tar zxvf fastjet-3.3.4.tar.gz
   cd $SUEP_BASE/fastjet-3.3.4
@@ -25,4 +26,13 @@ then
   make check
   make install
   cd $SUEP_BASE
-fi
+  exit 1
+}
+
+while getopts "fastjet" OPT; do
+  case $OPT in
+    fastjet)
+      FASTJET
+      ;;
+  esac
+done
